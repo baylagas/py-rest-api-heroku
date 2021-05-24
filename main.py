f1df14a16ebff464656c30935e697e5cace1ec85
baylagas <balbino_a@hotmail.com>
2021-05-23 22:20:03 -0600

    added flask cors dropped the debug=true

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from flask import Flask
 from flask_restful import Api
 from resource.video import Video
+from flask_cors import CORS
 
 app = Flask(__name__)
 api = Api(app)
+CORS(app)
 
 
 api.add_resource(Video, "/video/<int:id>")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
